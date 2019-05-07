d647a50ac3d006a90310d267a90824a89dc5dd13
Benjamin Tse <benjamin.l.tse@gmail.com>
2019-05-07 13:23:46 +1000

    Basic spare scoring

--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -1,13 +1,52 @@
 export class BowlingGame {
   constructor() {
     this.currentScore = 0;
+    this.frameNumber = 1;
+    this.bowlInFrame = 0;
+    this.numberOfPinsHitInFrame = 0;
+    this.isPreviousFrameSpare = false;
   }
 
   score() {
     return this.currentScore;
   }
 
+  frameNumber() {
+    return this.frameNumber;
+  }
+
   roll(noOfPins) {
+    ++this.bowlInFrame;
+    this.numberOfPinsHitInFrame += noOfPins;
+
     this.currentScore += noOfPins;
+
+    if (this.isScoringASpare()) {
+      this.currentScore += noOfPins;
+    }
+
+    if (this.isEndOfFrame()) {
+      if (this.isCurrentFrameSpare()) {
+        this.isPreviousFrameSpare = true;
+      }
+      this.bowlInFrame = 0;
+      this.numberOfPinsHitInFrame = 0;
+    }
+  }
+
+  isPreviousFrameASpare() {
+    return this.isPreviousFrameSpare;
+  }
+
+  isScoringASpare() {
+    return this.bowlInFrame === 1 && this.isPreviousFrameASpare();
+  }
+
+  isCurrentFrameSpare() {
+    return this.bowlInFrame === 2 && this.numberOfPinsHitInFrame === 10;
+  }
+
+  isEndOfFrame() {
+    return this.bowlInFrame === 2 || this.numberOfPinsHitInFrame === 10;
   }
 }
--- test/BowlingGame.spec.js
+++ test/BowlingGame.spec.js
@@ -1,21 +1,59 @@
 import { BowlingGame } from '../src/BowlingGame';
 
 describe('Given a BowlingGame', () => {
-  const bowlingGame = new BowlingGame();
+  let bowlingGame;
+  beforeEach(() => {
+    bowlingGame = new BowlingGame();
+  });
   
   describe('when the game is started', () => {
     test('then the initial score is 0', () => {
       expect(bowlingGame.score()).toBe(0);
     });
+
+    test('then the previous frame is not a spare', () => {
+      expect(bowlingGame.isPreviousFrameASpare()).toBe(false);
+    });
   });
 
-  describe('when a ball hits 5 pins', () => {
+  describe('when the player hits 5 pins', () => {
     beforeEach(() => {
       bowlingGame.roll(5);
     });
 
     test('then the score is 5', () => {
       expect(bowlingGame.score()).toBe(5);
+    });
+
+    describe('and then 4 pins', () => {
+      beforeEach(() => {
+        bowlingGame.roll(4);
+      });
+
+      test('then the score is 9', () => {
+        expect(bowlingGame.score()).toBe(9);
+      });
+    });
+  });
+
+  describe('when the rolls a 4 and a 6', () => {
+    beforeEach(() => {
+      bowlingGame.roll(4);
+      bowlingGame.roll(6);
+    });
+
+    test('then player has scored a spare', () => {
+      expect(bowlingGame.isPreviousFrameASpare()).toBe(true);
+    });
+
+    describe('and then in the next bowl hits 5 pins', () => {
+      beforeEach(() => {
+        bowlingGame.roll(5);
+      });
+
+      test('then the score is the sum of the pins knocked down plus the number knocked down in the next bowl', () => {
+        expect(bowlingGame.score()).toBe(20);
+      });
     });
   });
 });
