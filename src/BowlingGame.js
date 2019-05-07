6bd730fac1134e0e9c5090abf41737f6abec60a1
Benjamin Tse <benjamin.l.tse@gmail.com>
2019-05-07 12:46:41 +1000

    Implement startup and first roll

--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -1,4 +1,13 @@
 export class BowlingGame {
   constructor() {
+    this.currentScore = 0;
+  }
+
+  score() {
+    return this.currentScore;
+  }
+
+  roll(noOfPins) {
+    this.currentScore += noOfPins;
   }
 }
--- test/BowlingGame.spec.js
+++ test/BowlingGame.spec.js
@@ -1,10 +1,22 @@
 import { BowlingGame } from '../src/BowlingGame';
 
-describe('When a BowlingGame is started', () => {
+describe('Given a BowlingGame', () => {
   const bowlingGame = new BowlingGame();
   
-  test('then the initial score is 0', () => {
-    expect(true).toBe(true);
+  describe('when the game is started', () => {
+    test('then the initial score is 0', () => {
+      expect(bowlingGame.score()).toBe(0);
+    });
+  });
+
+  describe('when a ball hits 5 pins', () => {
+    beforeEach(() => {
+      bowlingGame.roll(5);
+    });
+
+    test('then the score is 5', () => {
+      expect(bowlingGame.score()).toBe(5);
+    });
   });
 });
 
