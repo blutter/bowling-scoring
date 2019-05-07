acaab7a92252253463a2a0516f6f3a2de3d0eb5d
Benjamin Tse <benjamin.l.tse@gmail.com>
2019-05-07 12:02:05 +1000

    Scaffold repo

---
+++ test/BowlingGame.spec.js
@@ -0,0 +1,10 @@
+import { BowlingGame } from '../src/BowlingGame';
+
+describe('When a BowlingGame is started', () => {
+  const bowlingGame = new BowlingGame();
+  
+  test('then the initial score is 0', () => {
+    expect(true).toBe(true);
+  });
+});
+
